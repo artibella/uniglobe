60a9ec6d247db781d86924338ce1d24201aee880
Richard Bausek <richardb@uniform.dev>
2025-01-16 13:19:41 +0100

    add Insights to Context config

--- src/context/createUniformContext.ts
+++ src/context/createUniformContext.ts
@@ -5,6 +5,7 @@ import {
   ContextPlugin,
   enableDebugConsoleLogDrain,
   enableContextDevTools,
+  enableUniformInsights,
 } from '@uniformdev/context';
 import { NextCookieTransitionDataStore } from '@uniformdev/context-next';
 
@@ -12,6 +13,20 @@ import manifest from './manifest.json';
 
 export default function createUniformContext(serverContext?: NextPageContext): Context {
   const plugins: ContextPlugin[] = [enableContextDevTools(), enableDebugConsoleLogDrain('debug')];
+
+  // adding the insights plugin client-side only
+  if (typeof window !== 'undefined' && window.document) {
+    plugins.push(
+      // running against a local endpoint, will use edge middleware to rewrite to the actual endpoint
+      enableUniformInsights({
+        endpoint: {
+          type: 'proxy',
+          path: '/api/analytics',
+        },
+      })
+    );
+  }
+
   const context = new Context({
     defaultConsent: true,
     manifest: manifest as ManifestV2,
