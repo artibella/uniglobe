ffc22be2d8185fabe5f4278463d148adad4a12fb
Richard Bausek <richardb@uniform.dev>
2025-09-12 14:30:55 +0200

    Add preview support for compositions without attached nodes

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,7 +1,13 @@
 import { createPreviewHandler } from '@uniformdev/canvas-next';
 
+const PLAYGROUND_PATH = '/playground';
+
 // Preview Mode, more info https://nextjs.org/docs/advanced-features/preview-mode
 export default createPreviewHandler({
   secret: () => process.env.UNIFORM_PREVIEW_SECRET || 'hello-world',
-  playgroundPath: '/playground',
+  playgroundPath: PLAYGROUND_PATH,
+  resolveFullPath: ({ path }) => {
+    const playgroundPath = PLAYGROUND_PATH;
+    return path?.length ? path : playgroundPath;
+  },
 });
