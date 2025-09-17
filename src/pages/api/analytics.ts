1f57ab1658fd9b2a2ec951a984cbd36286e3f28c
Artem Miroshnyk <miroshnik1992@gmail.com>
2025-09-17 13:16:11 +0200

    chore: upgrade to latest insight alpha package

--- src/pages/api/analytics.ts
+++ src/pages/api/analytics.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { createBackendInsightsProxyHandler } from '@uniformdev/context-insights/proxy';
 
 if (!process.env.NEXT_PUBLIC_INSIGHTS_API_URL) {
   throw new Error('NEXT_PUBLIC_INSIGHTS_API_URL is not set');
@@ -7,20 +8,14 @@ if (!process.env.NEXT_PUBLIC_UNIFORM_INSIGHTS_KEY) {
   throw new Error('NEXT_PUBLIC_UNIFORM_INSIGHTS_KEY is not set');
 }
 
+// Create backend proxy handler from environment variables
+const proxyHandler = createBackendInsightsProxyHandler({
+  apiHost: process.env.NEXT_PUBLIC_INSIGHTS_API_URL!,
+  apiKey: process.env.NEXT_PUBLIC_UNIFORM_INSIGHTS_KEY!,
+});
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const destination = new URL(process.env.NEXT_PUBLIC_INSIGHTS_API_URL!);
-  destination.pathname = '/v0/events';
-  destination.searchParams.set('name', 'events');
+  const proxyResponse = await proxyHandler.handleRequest(req.body);
 
-  const response = await fetch(destination.toString(), {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_UNIFORM_INSIGHTS_KEY}`,
-      'Content-Type': 'application/x-ndjson',
-    },
-    body: req.body,
-  });
-
-  const json = await response.json();
-  res.status(response.status).json(json);
+  res.status(proxyResponse.status).json(JSON.parse(proxyResponse.body));
 }
